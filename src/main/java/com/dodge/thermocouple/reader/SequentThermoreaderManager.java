52eb39972ad282d1426ff936c63ec919d8a2b0c4
dodgemich <dodge.mich@gmail.com>
2023-12-02 16:57:24 -0600

    round

--- src/main/java/com/dodge/thermocouple/reader/SequentThermoreaderManager.java
+++ src/main/java/com/dodge/thermocouple/reader/SequentThermoreaderManager.java
@@ -1,14 +1,16 @@
 package com.dodge.thermocouple.reader;
 
 import java.io.IOException;
+import java.text.DecimalFormat;
 
 import org.apache.commons.io.IOUtils;
 
 import com.dodge.thermocouple.reader.pojo.TemperatureReading;
 
 public class SequentThermoreaderManager {
-	
-	
+
+	private static final DecimalFormat FORMAT = new DecimalFormat("0.0");
+
 	public TemperatureReading getTemps() throws IOException {
 		TemperatureReading r = new TemperatureReading();
 		r.addTemperature(getReading("1"));
@@ -18,17 +20,17 @@ public class SequentThermoreaderManager {
 		r.addTemperature(getReading("5"));
 		return r;
 	}
-	
-	
+
 	private Double getReading(String id) throws IOException {
 		Process p = new ProcessBuilder("smtc", "0", "read", id).start();
 		String stdout = IOUtils.toString(p.getInputStream());
 		System.out.println(stdout);
-		Double c =  Double.parseDouble(stdout);
-		return (c*9/5)+32.0;
-	
+		Double c = Double.parseDouble(stdout);
+		Double f = (c * 9 / 5) + 32.0;
+		return Double.parseDouble(FORMAT.format(f));
+
 	}
-	
+
 	public static void main(String... strings) throws IOException {
 		SequentThermoreaderManager m = new SequentThermoreaderManager();
 		m.getTemps();
