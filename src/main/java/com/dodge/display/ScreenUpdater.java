b507920632765a5fe844ab45a1bf5e6b4c34b933
Mike Dodge <mike@lonewolfworks.com>
2023-12-03 20:36:58 -0500

    split

---
+++ src/main/java/com/dodge/display/ScreenUpdater.java
@@ -0,0 +1,81 @@
+package com.dodge.display;
+
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.time.Duration;
+
+import org.apache.commons.io.IOUtils;
+
+import com.pi4j.Pi4J;
+import com.pi4j.context.Context;
+import com.pi4j.library.pigpio.PiGpio;
+import com.pi4j.plugin.linuxfs.provider.i2c.LinuxFsI2CProvider;
+import com.pi4j.plugin.pigpio.provider.gpio.digital.PiGpioDigitalInputProvider;
+import com.pi4j.plugin.pigpio.provider.gpio.digital.PiGpioDigitalOutputProvider;
+import com.pi4j.plugin.pigpio.provider.pwm.PiGpioPwmProvider;
+import com.pi4j.plugin.pigpio.provider.serial.PiGpioSerialProvider;
+import com.pi4j.plugin.pigpio.provider.spi.PiGpioSpiProvider;
+import com.pi4j.plugin.raspberrypi.platform.RaspberryPiPlatform;
+
+public class ScreenUpdater {
+	private Context pi4j;
+	private static final DecimalFormat FORMAT = new DecimalFormat("0.0");
+
+	
+	public ScreenUpdater() {
+		PiGpio piGpio = PiGpio.newNativeInstance();
+
+		pi4j = Pi4J.newContextBuilder()
+				.noAutoDetect()
+				.add(new RaspberryPiPlatform() {
+					@Override
+					protected String[] getProviders() {
+						return new String[]{};
+					}
+				})
+				.add(PiGpioDigitalInputProvider.newInstance(piGpio),
+						PiGpioDigitalOutputProvider.newInstance(piGpio),
+						PiGpioPwmProvider.newInstance(piGpio),
+						PiGpioSerialProvider.newInstance(piGpio),
+						PiGpioSpiProvider.newInstance(piGpio),
+						LinuxFsI2CProvider.newInstance()
+				)
+				.build();
+	}
+	
+	
+	private Double getReading(String id) throws IOException {
+		Process p = new ProcessBuilder("/usr/local/bin/smtc", "0", "read", id).start();
+		String stdout = IOUtils.toString(p.getInputStream());
+		Double c = Double.parseDouble(stdout);
+		Double f = (c * 9 / 5) + 32.0;
+		if (id.equals("4")) {
+			// house temp is off
+			f = f * .97;
+		}
+		return Double.parseDouble(FORMAT.format(f));
+
+	}
+	
+	
+    public void execute(Context pi4j) {
+        System.out.println("LCD demo started");
+
+        //Create a Component, with amount of ROWS and COLUMNS of the device
+        //LcdDisplay lcd = new LcdDisplay(pi4j); //2x16 is default
+        LcdDisplay lcd = new LcdDisplay(pi4j, 4, 20);
+
+        // Write text to specific position
+        lcd.displayLineOfText("Hello" , 0);
+        lcd.displayLineOfText("World!", 1, 3);
+
+
+
+        lcd.clearDisplay();
+
+        lcd.centerTextInLine("Hi", 0);
+
+
+    }
+
+}
--- src/main/java/com/dodge/thermocouple/daemon/LoggerDaemon.java
+++ src/main/java/com/dodge/thermocouple/daemon/LoggerDaemon.java
@@ -9,16 +9,14 @@ import com.dodge.furnace.Pump;
 import com.dodge.thermocouple.persistence.CloudwatchPersister;
 import com.dodge.thermocouple.reader.SequentThermoreaderManager;
 import com.dodge.thermocouple.reader.pojo.TemperatureReading;
-
-import com.pi4j.component.lcd.LCDTextAlignment;
-import com.pi4j.component.lcd.impl.GpioLcdDisplay;
-import com.pi4j.io.gpio.GpioController;
-import com.pi4j.io.gpio.GpioFactory;
-import com.pi4j.io.gpio.RaspiPin;
+import com.pi4j.Pi4J;
+import com.pi4j.library.pigpio.PiGpio;
+import com.pi4j.plugin.pigpio.provider.gpio.digital.PiGpioDigitalInputProvider;
+import com.pi4j.plugin.pigpio.provider.gpio.digital.PiGpioDigitalOutputProvider;
+import com.pi4j.plugin.raspberrypi.platform.RaspberryPiPlatform;
 
 public class LoggerDaemon implements Runnable {
 
-	private static final DecimalFormat FORMAT = new DecimalFormat("0.0");
 
 	public void run() {
 		System.out.println("Fetching...");
@@ -63,51 +61,6 @@ public class LoggerDaemon implements Runnable {
 
 	}
 
-	private Double getReading(String id) throws IOException {
-		Process p = new ProcessBuilder("/usr/local/bin/smtc", "0", "read", id).start();
-		String stdout = IOUtils.toString(p.getInputStream());
-		Double c = Double.parseDouble(stdout);
-		Double f = (c * 9 / 5) + 32.0;
-		if (id.equals("4")) {
-			// house temp is off
-			f = f * .97;
-		}
-		return Double.parseDouble(FORMAT.format(f));
-
-	}
-
-	public void updateDisplay() {
-
-
-        System.out.println("16X2 LCD Example with Raspberry Pi using Pi4J and JAVA");
-
-        // create gpio controller
-        final GpioController gpio = GpioFactory.getInstance();
-
-        // initialize LCD
-        final GpioLcdDisplay lcd = new GpioLcdDisplay(2,    // number of row supported by LCD
-                                                16,       // number of columns supported by LCD
-                                                RaspiPin.GPIO_23,  // LCD RS pin
-                                                RaspiPin.GPIO_24,  // LCD strobe pin
-                                                RaspiPin.GPIO_04,  // LCD data bit D4
-                                                RaspiPin.GPIO_17,  // LCD data bit D5
-                                                RaspiPin.GPIO_27,  // LCD data bit D6
-                                                RaspiPin.GPIO_22); // LCD data bit D7
-
-        lcd.clear();
-        Thread.sleep(1000);
-
-        lcd.write(LCD_ROW_1, "WeArGenius");
-        lcd.write(LCD_ROW_2, " ???");
-
-        // Thread.sleep(2000);
-        // for (String ipAddress : NetworkInfo.getIPAddresses()){
-        //     System.out.println("IP Addresses      :  " + ipAddress);
-        //     lcd.writeln(LCD_ROW_2,ipAddress,LCDTextAlignment.ALIGN_CENTER);
-        // }
-
-        gpio.shutdown();
-	}
 
 	public static void main(String[] args) {
 		LoggerDaemon d = new LoggerDaemon();
@@ -115,7 +68,7 @@ public class LoggerDaemon implements Runnable {
 			d.run();
 			System.exit(0);
 		} else {
-			d.updateDisplay();
+			//d.updateDisplay();
 		}
 	}
 
